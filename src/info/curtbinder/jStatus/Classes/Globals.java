43a64e9873d352d45f100fe32fcb156a62fe1bc1
Curt Binder <curt.binder@gmail.com>
2011-08-30 15:41:58 -0400

    Updated version and legal information

--- src/info/curtbinder/jStatus/Classes/Globals.java
+++ src/info/curtbinder/jStatus/Classes/Globals.java
@@ -4,7 +4,7 @@ public class Globals {
 	public static final String WifiActionCommand = "WIFI";
 	public static final String ComActionCommand = "COM";
 	public static final String appTitle = "ReefAngel Status";
-	public static final int versionMajor = 2;
+	public static final int versionMajor = 1;
 	public static final int versionMinor = 0;
 	public static final int versionRevision = 0;
 	public static final String versionBuild = "alpha 1";
@@ -16,18 +16,7 @@ public class Globals {
 		"Curt Binder",
 		"Dave Molton"
 	};
-	public static final String legal = "This is a long line of text that is repeated over and over.  " +
-	"This is a long line of text that is repeated over and over.  " + 
-	"This is a long line of text that is repeated over and over.  " +
-	"This is a long line of text that is repeated over and over.  " +
-	"This is a long line of text that is repeated over and over.  " +
-	"This is a long line of text that is repeated over and over.  " +
-	"This is a long line of text that is repeated over and over.  " +
-	"This is a long line of text that is repeated over and over.  " +
-	"This is a long line of text that is repeated over and over.  " +
-	"This is a long line of text that is repeated over and over.  " +
-	"This is a long line of text that is repeated over and over.  " +
-	"This is a long line of text that is repeated over and over.  " +
-	"This is a long line of text that is repeated over and over.  " +
-	"This is a long line of text that is repeated over and over.  ";
+	public static final String legal = "This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. " + 
+	"To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative Commons, 444 Castro Street, " + 
+	"Suite 900, Mountain View, California, 94041, USA.";
 }
